dde6f4192fdd704710748e4a4ed715e7c1f67adb
ManoLo grajales <manolo_feo@hotmail.com>
2019-11-12 20:42:56 -0600

    lel

---
+++ ProyectoEmail.Android/SQLiteDB.cs
@@ -0,0 +1,21 @@
+﻿
+using Xamarin.Forms;
+using ProyectoEmail.Droid;
+using ProyectoEmail.Interfaces;
+using SQLite;
+using System.IO;
+
+[assembly: Dependency(typeof(SQLiteDB))]
+namespace ProyectoEmail.Droid
+{
+    public class SQLiteDB : ISQLiteDB
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "DB.db3");
+            return new SQLiteAsyncConnection(path);
+
+        }
+    }
+}
---
+++ ProyectoEmail/Interfaces/ISQLiteDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace ProyectoEmail.Interfaces
+{
+  
+        public interface ISQLiteDB
+        {
+        SQLiteAsyncConnection GetConnection();
+        }
+    }
+
